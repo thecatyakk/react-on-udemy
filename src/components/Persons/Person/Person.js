ba2ea21830c51e2a7ba90537da32533f49b7e250
anawat wannapila <knownamenow@gmail.com>
2018-06-03 12:59:30 +0700

    depp component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -3,7 +3,8 @@ import React from 'react';
 import classes from './Person.css';
 
 const person = (props) => {
-    return <div className={classes.Person}>
+    return (
+      <div className={classes.Person}>
         <p onClick={props.click}>
           I'm {props.name}
           and I am {props.age}
@@ -11,7 +12,7 @@ const person = (props) => {
         </p>
         <p>{props.children}</p>
         <input type="text" onChange={props.changed} value={props.name} />
-      </div>;
+      </div>);
 };
 
 export default person;
